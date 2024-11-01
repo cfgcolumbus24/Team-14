3299f13dfc7efa567494e9c89a74f9d1cec018da
Pranav Prakash <pranavprakash26@gmail.com>
2024-11-01 18:07:21 -0400

    Create IT-Admin.js
    Made the It-Admin file, will work on this later if we have time

---
+++ client/src/components/IT-Admin.js
@@ -0,0 +1,13 @@
+// src/components/ITAdminPage.js
+import React from "react";
+
+function ITAdminPage() {
+  return (
+    <div>
+      <h2>IT-Admin Page</h2>
+      <p>Welcome to the IT-Admin dashboard section.</p>
+    </div>
+  );
+}
+
+export default ITAdminPage;

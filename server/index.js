e27c19d5c269ca57b1dab4c180e0a8018a6b06fc
nihar <nihar.kodkani@gmail.com>
2024-11-01 18:17:57 -0400

    added routes

--- server/index.js
+++ server/index.js
@@ -5,3 +5,14 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use('/api')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+  
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+  

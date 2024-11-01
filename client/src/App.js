aabad108414e8d60cad225fcf8556a7194d27042
Nicholas Cope <nicholascope2709@gmail.com>
2024-11-01 18:14:00 -0400

    Basic dashboard view

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <Dashboard />
     </div>
   );
 }

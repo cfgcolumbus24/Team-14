99066401be1958955d2a06002e5d376aef8f187b
Nicholas Cope <nicholascope2709@gmail.com>
2024-11-01 18:55:41 -0400

    Added styling for main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div>
-      <Dashboard />;
+    <div className="app-container">
+      <header className="app-header">
+        <div className="header-content">
+          <img
+            src="netcare_logo.png"
+            alt="Netcare Access Logo"
+            className="logo"
+          />{" "}
+          <h1>Netcare Access Dashboard</h1>
+          <p className="header-subtitle">
+            Empowering Behavioral Healthcare and Emotional Support
+          </p>
+        </div>
+      </header>
+      <main className="app-main">
+        <Dashboard />
+      </main>
+      <footer className="app-footer">
+        <p>&copy; {new Date().getFullYear()} Netcare Access</p>
+      </footer>
     </div>
   );
 }

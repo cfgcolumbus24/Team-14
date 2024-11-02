5910f422c435b2f248a2d68397155bdcdc5e87ea
Victor <victorfawole@gmail.com>
2024-11-02 05:16:40 -0400

    Ugh

--- server/services/langchain.js
+++ server/services/langchain.js
@@ -2,25 +2,35 @@ import dotenv from 'dotenv';
 import express from "express";
 dotenv.config(); // Explicit path if needed
 
-import { ChatOpenAI } from "@langchain/openai";
+import OpenAI from 'openai';
 import { MongoClient } from 'mongodb';
+import { parse } from 'path';
 
-console.log('Loaded API Key:', process.env["OPENAI_API_KEY"]); // Debugging log
-const chatModel = new ChatOpenAI({
+const openAIClient = new OpenAI({
     apiKey: "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"
-});
+})
+
+const messagesInput = [
+    {
+        role: "system", 
+        content: "You are a helpful language parsing assistant. Use the supplied tools to assist the user. In your mongodb database, you have the collections, ehr_collections, hris_collections, mitel_collections, and quickbooks_collections. The ehr relates to patients, Mitel phone numbers and calls."
+    },
+];
 
 const generateMongoQuery = async(naturalQuery) => {
-    const llmResponse = await chatModel.call(`Generate a MongoDB find query for this request: "${naturalQuery}"`);
-    console.log('Generated MongoDB Query:', llmResponse);
+    content = naturalQuery
+    messagesInput.push({role: "user", content});
 
-    try {
-        const parsedQuery = JSON.parse(llmResponse);
-        return parsedQuery;
-    } catch (error) {
-        console.error('Error parsing the generated MongoDB query:', error);
-        throw new Error('Invalid query format generated.');
-    }
+    const response = await openAIClient.chat.completions.create({
+        model: "gpt-4o-mini-2024-07-18",
+        messages: messagesInput,
+        tool_choice: "required", // Must always return function
+        tools: tools,
+    })
+
+    const dbQuery = response.choices[0].message
+    console.log(dbQuery)
+    return dbQuery
 }
 
 const executeMongoQuery = async(dbName, collectionName, query) => {

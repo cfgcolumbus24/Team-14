2d29ace7004d44e72c4ea06308066826d82f36ce
evan_mccabe <mccabe.265@buckeyemail.osu.edu>
2024-11-02 01:26:45 -0400

    Fixed queries to use set parameters

--- client/src/components/CliniciansVisuals.js
+++ client/src/components/CliniciansVisuals.js
@@ -3,274 +3,34 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 import "../styles/CliniciansVisuals.css";
 import Button from "./Button";
 
+
 function CliniciansVisuals(props) {
   // Sample data as an array of objects
-  const patientData = [
-    {
-      PatientID: "PAT0001",
-      Age: 25,
-      Gender: "Male",
-      Diagnosis: "Asthma",
-      Medication: "Albuterol",
-      VisitDate: "2024-05-16",
-      LabResult: 100.1,
-      Insurance: "Uninsured",
-    },
-    {
-      PatientID: "PAT0002",
-      Age: 54,
-      Gender: "Female",
-      Diagnosis: "COPD",
-      Medication: "Lisinopril",
-      VisitDate: "2024-12-06",
-      LabResult: 60.85,
-      Insurance: "Medicare",
-    },
-    {
-      PatientID: "PAT0003",
-      Age: 62,
-      Gender: "Female",
-      Diagnosis: "COPD",
-      Medication: "Lisinopril",
-      VisitDate: "2024-07-30",
-      LabResult: 50.98,
-      Insurance: "Medicare",
-    },
-    {
-      PatientID: "PAT0004",
-      Age: 57,
-      Gender: "Male",
-      Diagnosis: "Hypertension",
-      Medication: "Atorvastatin",
-      VisitDate: "2024-07-09",
-      LabResult: 115.3,
-      Insurance: "Medicaid",
-    },
-    {
-      PatientID: "PAT0005",
-      Age: 58,
-      Gender: "Female",
-      Diagnosis: "COPD",
-      Medication: "Metformin",
-      VisitDate: "2024-04-01",
-      LabResult: 70.92,
-      Insurance: "Private",
-    },
-    {
-      PatientID: "PAT0006",
-      Age: 54,
-      Gender: "Female",
-      Diagnosis: "Asthma",
-      Medication: "Albuterol",
-      VisitDate: "2024-08-16",
-      LabResult: 129.82,
-      Insurance: "Uninsured",
-    },
-    {
-      PatientID: "PAT0007",
-      Age: 37,
-      Gender: "Male",
-      Diagnosis: "Diabetes",
-      Medication: "Lisinopril",
-      VisitDate: "2024-10-11",
-      LabResult: 87.51,
-      Insurance: "Uninsured",
-    },
-    {
-      PatientID: "PAT0008",
-      Age: 54,
-      Gender: "Male",
-      Diagnosis: "COPD",
-      Medication: "Atorvastatin",
-      VisitDate: "2024-06-09",
-      LabResult: 101.15,
-      Insurance: "Private",
-    },
-    {
-      PatientID: "PAT0009",
-      Age: 68,
-      Gender: "Female",
-      Diagnosis: "COPD",
-      Medication: "Aspirin",
-      VisitDate: "2024-08-22",
-      LabResult: 68.27,
-      Insurance: "Private",
-    },
-    {
-      PatientID: "PAT0010",
-      Age: 90,
-      Gender: "Female",
-      Diagnosis: "Hypertension",
-      Medication: "Lisinopril",
-      VisitDate: "2024-03-24",
-      LabResult: 92.84,
-      Insurance: "Medicaid",
-    },
-    {
-      PatientID: "PAT0011",
-      Age: 70,
-      Gender: "Male",
-      Diagnosis: "Heart Disease",
-      Medication: "Lisinopril",
-      VisitDate: "2024-05-14",
-      LabResult: 103.81,
-      Insurance: "Medicare",
-    },
-    {
-      PatientID: "PAT0012",
-      Age: 49,
-      Gender: "Female",
-      Diagnosis: "Asthma",
-      Medication: "Lisinopril",
-      VisitDate: "2024-03-08",
-      LabResult: 90.42,
-      Insurance: "Medicare",
-    },
-    {
-      PatientID: "PAT0013",
-      Age: 69,
-      Gender: "Male",
-      Diagnosis: "COPD",
-      Medication: "Aspirin",
-      VisitDate: "2024-12-02",
-      LabResult: 111.9,
-      Insurance: "Private",
-    },
-    {
-      PatientID: "PAT0014",
-      Age: 66,
-      Gender: "Female",
-      Diagnosis: "Diabetes",
-      Medication: "Albuterol",
-      VisitDate: "2024-03-04",
-      LabResult: 81.0,
-      Insurance: "Medicaid",
-    },
-    {
-      PatientID: "PAT0015",
-      Age: 81,
-      Gender: "Female",
-      Diagnosis: "Diabetes",
-      Medication: "Lisinopril",
-      VisitDate: "2024-01-05",
-      LabResult: 130.47,
-      Insurance: "Uninsured",
-    },
-    {
-      PatientID: "PAT0016",
-      Age: 39,
-      Gender: "Male",
-      Diagnosis: "Hypertension",
-      Medication: "Aspirin",
-      VisitDate: "2024-12-31",
-      LabResult: 148.74,
-      Insurance: "Medicaid",
-    },
-    {
-      PatientID: "PAT0017",
-      Age: 81,
-      Gender: "Male",
-      Diagnosis: "Hypertension",
-      Medication: "Aspirin",
-      VisitDate: "2024-12-01",
-      LabResult: 110.41,
-      Insurance: "Medicaid",
-    },
-    {
-      PatientID: "PAT0018",
-      Age: 23,
-      Gender: "Male",
-      Diagnosis: "COPD",
-      Medication: "Atorvastatin",
-      VisitDate: "2024-10-12",
-      LabResult: 54.88,
-      Insurance: "Medicaid",
-    },
-    {
-      PatientID: "PAT0019",
-      Age: 54,
-      Gender: "Female",
-      Diagnosis: "COPD",
-      Medication: "Atorvastatin",
-      VisitDate: "2024-09-20",
-      LabResult: 130.22,
-      Insurance: "Medicare",
-    },
-    {
-      PatientID: "PAT0020",
-      Age: 29,
-      Gender: "Female",
-      Diagnosis: "Hypertension",
-      Medication: "Atorvastatin",
-      VisitDate: "2024-01-07",
-      LabResult: 65.11,
-      Insurance: "Uninsured",
-    },
-    {
-      PatientID: "PAT0021",
-      Age: 64,
-      Gender: "Male",
-      Diagnosis: "COPD",
-      Medication: "Aspirin",
-      VisitDate: "2024-04-13",
-      LabResult: 138.28,
-      Insurance: "Medicaid",
-    },
-    {
-      PatientID: "PAT0022",
-      Age: 27,
-      Gender: "Male",
-      Diagnosis: "Asthma",
-      Medication: "Atorvastatin",
-      VisitDate: "2024-05-29",
-      LabResult: 146.01,
-      Insurance: "Medicare",
-    },
-    {
-      PatientID: "PAT0023",
-      Age: 78,
-      Gender: "Female",
-      Diagnosis: "Heart Disease",
-      Medication: "Albuterol",
-      VisitDate: "2024-02-05",
-      LabResult: 64.72,
-      Insurance: "Private",
-    },
-    {
-      PatientID: "PAT0024",
-      Age: 88,
-      Gender: "Male",
-      Diagnosis: "COPD",
-      Medication: "Lisinopril",
-      VisitDate: "2024-06-02",
-      LabResult: 144.82,
-      Insurance: "Uninsured",
-    },
-    {
-      PatientID: "PAT0025",
-      Age: 31,
-      Gender: "Female",
-      Diagnosis: "Asthma",
-      Medication: "Aspirin",
-      VisitDate: "2024-01-28",
-      LabResult: 63.88,
-      Insurance: "Private",
-    },
-  ];
 
-  console.log(fetch("http://localhost:3001/api/data"));
+  React.useEffect(() => {
+    fetch(`http://localhost:3001/api/data/?type=EHR`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setPatientData(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
-  const [realData, setRealData] = React.useState([]);
+  const [patientData, setPatientData] = React.useState([]);
 
-  console.log(realData);
+  // Data for the patients count by start visit month and year chart
+  const visitDateCounts = patientData.reduce((acc, patient) => {
+    const visitDate = new Date(patient.StartVisitDate);
+    const monthYear = `${visitDate.getMonth() + 1}/${visitDate.getFullYear()}`;
+    acc[monthYear] = (acc[monthYear] || 0) + 1;
+    return acc;
+  }, {});
 
-  // data for the total patients chart
-  const numPatients = [
-    {
-      name: "Total Patients",
-      count: patientData.length,
-    },
-  ];
+  const visitDateData = Object.keys(visitDateCounts).map((monthYear) => ({
+    name: monthYear,
+    count: visitDateCounts[monthYear],
+  }));
 
   // Data for the patients count by diagnosis chart
   const diagnosisCounts = patientData.reduce((acc, patient) => {
@@ -289,7 +49,7 @@ function CliniciansVisuals(props) {
       <p>Welcome to the Clinicians dashboard section.</p>
       <div style={{ display: "flex", justifyContent: "space-around" }}>
         <div style={{ width: "600px" }}>
-          <BarChart width={600} height={300} data={numPatients}>
+          <BarChart width={600} height={300} data={visitDateData}>
             <XAxis dataKey="name" />
             <YAxis />
             <Tooltip />
@@ -309,7 +69,6 @@ function CliniciansVisuals(props) {
       </div>
       <Button />
     </div>
-    
   );
 }
 

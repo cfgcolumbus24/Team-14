d38b465fc6da06923f9fb9b8bd396a62b4e03415
nihar <nihar.kodkani@gmail.com>
2024-11-01 20:33:51 -0400

    test ehr

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -1,15 +1,22 @@
 import express from express;
 
+const testEHR = require('../data/testData.json');
+
 const getData = async (req, res) => {
-    const {type} = req.body;
-    if (type === 'EHR') {
-        //res.send('EHR data');
-    } else if (type === 'Mytel') {
-        //res.send('Phone data');
-    } else if (type === 'quickBooks') {
-        //res.send('Phone data');
-    } else if (type === '')
-    res.send('Data route');
+    try {
+        const {type} = req.body;
+        if (type === 'EHR') {
+            res.send(testEHR);
+        } else if (type === 'Mytel') {
+            //res.send('Phone data');
+        } else if (type === 'quickBooks') {
+            //res.send('Phone data');
+        } else if (type === '')
+        res.send('Data route');
+    } catch (err) {
+        console.log(err);
+        res.send('An error occurred');
+    }
 }
 
 const getQuery = async (req, res) => {

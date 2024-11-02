1697a28386e74745ca3a5198a8bac2d2b9b1ee72
evan_mccabe <mccabe.265@buckeyemail.osu.edu>
2024-11-01 20:08:35 -0400

    Added second visualization

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Visualizations from './visualizations';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Visualizations />
   </React.StrictMode>
 );
 
